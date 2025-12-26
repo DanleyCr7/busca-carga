2d673e2df2ae6945d3fafc80bbac93e83b13da8a
Danrley Silva <danrleysil46@gmail.com>
2025-12-26 10:26:20 -0300

    fix: privaci

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,11 +5,13 @@ import { Route, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
+import Privacidade from "./pages/privacidade";
 
 function Router() {
   return (
     <Switch>
       <Route path={"/"} component={Home} />
+      <Route path={"/privacidade"} component={Privacidade} />
       <Route path={"/404"} component={NotFound} />
       {/* Final fallback route */}
       <Route component={NotFound} />
